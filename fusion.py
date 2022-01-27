f29bcbc9cb9aa2e258207e18bdd4b6b561de8601
Sarah <94045273+Sarahgrnt@users.noreply.github.com>
2022-01-27 13:44:47 +0100

    Update fusion.py

--- fusion.py
+++ fusion.py
@@ -4,14 +4,13 @@ import os
 
 path= "..\..\data\\raw\\"
 all_filenames=glob.glob(os.path.join(path,"*.csv"))
-all_filenames.sort()
 all_df=[]
-print(all_filenames)
 df = pd.read_csv(all_filenames[0],sep=',')
 name=list(df.columns.values)
 for f in all_filenames:
     df=pd.read_csv(f,sep=',',names=name)
     all_df.append(df)
-merged_df =pd.concat(all_df,ignore_index=True,sort=True)
+merged_df =pd.concat(all_df,ignore_index=True)
 print(merged_df)
 merged_df.to_csv("netflix.csv")
+
