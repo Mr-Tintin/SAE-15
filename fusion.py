2695ff640bbaa73eedab3c827e5952c081f189f4
Sarah <94045273+Sarahgrnt@users.noreply.github.com>
2022-01-27 09:19:59 +0100

    Add files via upload

---
+++ fusion.py
@@ -0,0 +1,17 @@
+import glob
+import pandas as pd
+import os
+
+path= "..\..\data\\raw\\"
+all_filenames=glob.glob(os.path.join(path,"*.csv"))
+all_filenames.sort()
+all_df=[]
+print(all_filenames)
+df = pd.read_csv(all_filenames[0],sep=',')
+name=list(df.columns.values)
+for f in all_filenames:
+    df=pd.read_csv(f,sep=',',names=name)
+    all_df.append(df)
+merged_df =pd.concat(all_df,ignore_index=True,sort=True)
+print(merged_df)
+merged_df.to_csv("netflix.csv")
